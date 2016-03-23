27d01019dd80d885d96bd83116b1887a1d6b36c2
88Diego <digo_cb@hotmail.com>
2016-03-23 20:41:02 -0300

    Login PHP Fake

---
+++ templates/index.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+$logins = array (
+		'user1' => 'pass1',
+		'user2' => 'pass2',
+		'user3' => 'pass3'
+	);
+
+if (isset($_POST['btn_login'])) {
+	
+	if (isset($logins[$_POST['login']])) {
+		
+		if ($logins[$_POST['login']] == $_POST['senha']) {
+			
+			include('admin.php');
+			
+		} else {
+			
+			$msg = 'Usuário ou Senha Inválidos';
+				
+			include('login_template.php');				
+			
+		}
+	
+	} else {
+				
+		$msg = 'Usuário ou Senha Inválidos';
+				
+		include('login_template.php');			
+				
+	}
+	
+} else {
+	
+	include('login_template.php');	
+	
+}
+?>
